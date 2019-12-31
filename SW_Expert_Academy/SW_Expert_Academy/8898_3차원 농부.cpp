abd0b93c074fde69d600f35e09ade577f3d8c2eb
muk0920 <tkddn0928@naver.com>
2019-12-31 17:06:42 +0900

    문제 미 해결 - 진행중

---
+++ SW_Expert_Academy/SW_Expert_Academy/8898_3차원 농부.cpp
@@ -0,0 +1,11 @@
+// N 付府 家 . M 付府 富. 
+
+#define _CRT_SECURE_NO_WARNIGNS
+#include <iostream>
+using namespace std; 
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	freopen("input.txt", "r", stdin);
+}
