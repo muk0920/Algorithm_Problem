42765990724bfac25769ccb00b016b542dfffe29
muk0920 <tkddn0928@naver.com>
2020-01-09 22:20:04 +0900

    3812 미해결

---
+++ SW_Expert_Academy/SW_Expert_Academy/3812_호중이의 큐브색칠.cpp
@@ -0,0 +1,29 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <iostream>
+//#include <vector>
+//#include <queue>
+//
+//using namespace std;
+//int X, Y, Z, A, B, C, N;
+//int len[1001];
+//void BFS() {
+//
+//}
+//
+//int main() {
+//	ios::sync_with_stdio(false); 
+//	cin.tie(NULL); 
+//	cout.tie(NULL);
+//	freopen("input.txt", "r", stdin);
+//	int tc;
+//	cin >> tc; 
+//	for (int t = 1; t <= tc; t++) {
+//		 
+//		cin >> X >> Y >> Z >> A >> B >> C >> N; 
+//		BFS(A, B, C);
+//
+//		
+//		cout << "\n";
+//	}
+//
+//}
