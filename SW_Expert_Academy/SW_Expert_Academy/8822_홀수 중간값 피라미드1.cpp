189fe336401f5eefd4069fb7393353dfd5945d61
muk0920 <tkddn0928@naver.com>
2020-01-09 17:19:25 +0900

    2033 해결 / 8822 미해결

---
+++ SW_Expert_Academy/SW_Expert_Academy/8822_홀수 중간값 피라미드1.cpp
@@ -0,0 +1,21 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <iostream>
+//using namespace std;
+//int main() {
+//	ios::sync_with_stdio(false); 
+//	cin.tie(NULL); 
+//	cout.tie(NULL); 
+//	freopen("input.txt", "r", stdin);
+//	int tc; 
+//	cin >> tc; 
+//	for (int t = 1; t <= tc; t++) {
+//		int floor, num, diff; 
+//		cin >> floor >> num;
+//
+//		diff = floor / 2 + 1; 
+//		 
+//		cout << "#" << t << " ";
+//		if ( num >=diff && num <=2*floor-diff)	cout << "1\n";
+//		else cout << "0\n";
+//	}
+//}
