83ddb8272b94ff938e74aa702f6436024592ab1c
muk0920 <tkddn0928@naver.com>
2020-01-09 23:05:40 +0900

    1188 미해결

---
+++ BackJoon/BackJoon/1188_음식 평론가.cpp
@@ -0,0 +1,10 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <iostream>
+//using namespace std; 
+//int main() {
+//	int N, M; 
+//	cin >> N >> M;
+//	if (N == M)	cout << "0\n";
+//	else if (N == 1)	cout << M - 1 << "\n";
+//	else
+//}
