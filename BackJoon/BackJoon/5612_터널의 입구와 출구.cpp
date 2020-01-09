7c711dede26cbb5ccbcd04e285590f704188bcc3
muk0920 <tkddn0928@naver.com>
2020-01-09 22:27:40 +0900

    5612 해결

---
+++ BackJoon/BackJoon/5612_터널의 입구와 출구.cpp
@@ -0,0 +1,19 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <iostream>
+//using namespace std;
+//int main() {
+//	int n, m, input, output, max_num=0, num;
+//	cin >> n >> m; 
+//	num = m;
+//	for (int i = 0; i < n; i++) {
+//		cin >> input >> output;
+//		num = num + input - output; 
+//		if (num > max_num)	max_num = num; 
+//
+//		if (num < 0) {
+//			max_num = 0;
+//			break;
+//		}
+//	}
+//	cout << max_num << "\n";
+//}
